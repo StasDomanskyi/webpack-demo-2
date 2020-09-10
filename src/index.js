8d5d0b888996ce91044cadee48ed4428ba370815
unknown <Stanislav Domanskyi>
2020-09-10 11:08:01 +0300

    modification to next chapter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import _ from 'lodash';
-import Uniform from './uniform.ttf';
-import Samosan from './samosan.ttf';
-import './style.css';
-import Mountains from './mountains.jpg';
-import Dog from './dog.jpg';
-
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-
-  const myIcon = new Image();
-  myIcon.src = Dog;
-
-  element.appendChild(myIcon);
 
   return element;
 }
